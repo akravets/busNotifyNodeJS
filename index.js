f5cd9371ddb3576a16caab9343822e8cb97903f5
Alex Kravets <kravets.dev@gmail.com>
2019-06-03 16:27:01 -0400

    added data analyze

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const moment = require('moment')
 require('dotenv').config();
 
 const image = path.join('/home/dev/busNotifyData','image-capture.jpg')
+//const image = path.join('/home/dev/busNotifyData','bus.jpeg')
+const SCHOOL_BUS = "school bus"
 
 var rec = new CustomRecorder({
     url: 'rtsp://root:azr26p@192.168.1.29:554/stream1',
@@ -34,13 +36,33 @@ var isBusDetected = function(){
 
   visualRecognition.classify(params)
     .then(result => {
-      console.log(JSON.stringify(result, null, 2));
+      const classes = result.images[0].classifiers[0].classes;
+      console.log(classes)
+      
+    console.log(getClassWithMaxScore(classes));
+    if(getClassWithMaxScore(classes) == SCHOOL_BUS){
+      console.log("School bus is here!")
+    }
     })
     .catch(err => {
       console.log(err);
     });
 }
 
+const getClassWithMaxScore = function(classes){
+  let max = 0;
+  let maxIndex = 0;
+
+  for(const clazz of classes){
+    if(clazz.score > max){
+      max = clazz.score;
+    }
+  }
+  return classes.filter(c => c.score === max)[0].class;
+}
+
+//capture();
+
 setInterval(() => {
     capture();
-}, 1000);
+}, 5000);
